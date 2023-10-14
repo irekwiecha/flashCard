873b6b9becb5f4cc5726ea9c69cd19a979af7c2b
irekwiecha <irek88@gmail.com>
2023-10-14 21:10:04 +0200

    chore: initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = tk.Tk()
+window.title("FlashCard")
+window.config(padx=50, pady=50)
+
+
+window.mainloop()
